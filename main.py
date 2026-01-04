bab61cdb55c20f069a94bdadaeb02e475fed6fab
kunkaffa <kunkaffa3@gmail.com>
2026-01-05 01:45:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def display_header():
         f"{Fore.WHITE}",      
         f"{Fore.YELLOW}════════════════════════════════════════════════════════════════════════════",
     ]
-    for line in header_lines:
-        print(line)
+for line in header_lines:
+    print(line)
     # Versi dan URL
     print(f"{Fore.WHITE}{Style.BRIGHT}{' ' * 57}v.1.0")
     print(f"{Fore.CYAN}{Style.BRIGHT}{' ' * 16}https://kunkaffa@gmail.com")
